4091ce4cc36e179effe19e3dd4f57a377478ab4f
JoelVR17 <vermudo.com@gmail.com>
2022-02-11 18:40:42 -0600

    Primer Commit

---
+++ src/main/java/com/Tarea_dos/TareaDosApplication.java
@@ -0,0 +1,13 @@
+package com.Tarea_dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaDosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaDosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tarea_dos/TareaDosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tarea_dos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaDosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
